2348fe76a9b5a24d4c2bf89647d64e93dc74297c
Celis <dedsa2016@gmail.com>
2023-08-16 15:13:25 -0500

    Agregadas propiedades para formulario (No-ultimo)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './componetes/Header/Header.js';
+import Formulario from './componetes/Formulario/Formulario';
 
 function App() {
   return (
     <div>
       <Header/>
+      <Formulario/>
     </div>
   );
 }
